b840f48afe0c2d1764c237390c6dddd729cdf408
doniv <donivrecord@gmail.com>
2015-06-01 14:44:55 +0600

    Cleanup

--- test/test_smster.rb
+++ test/test_smster.rb
@@ -2,10 +2,12 @@ require 'minitest/autorun'
 require 'smster'
 
 class SmsterTest < Minitest::Test
-  def test_example
+  def test_should_send
     @statuses = Sms::STATUSES
+    phone = (99_999_999 * rand).to_s
+    attrs = { to: phone, text: 'i am smster!', mode: 'test' }
 
-    sms = Sms::Smsru.new(to: '79050958806', text: 'i am smster!!!')
+    sms = Sms::Smsru.new(attrs)
     sms.send_sms
 
     assert_equal @statuses[:sent], sms.status
